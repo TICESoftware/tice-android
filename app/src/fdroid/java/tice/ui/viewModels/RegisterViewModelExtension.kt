71dc34c1d852a9054e51888f3601bd8ec2622615
Fabio Tacke <fabio@tacke.berlin>
2021-11-01 15:46:19 +0100

    Fix compilation error in fdroid flavor

--- app/src/fdroid/java/tice/ui/viewModels/RegisterViewModelExtension.kt
+++ app/src/fdroid/java/tice/ui/viewModels/RegisterViewModelExtension.kt
@@ -1,8 +1,9 @@
 package tice.ui.viewModels
 
+import android.content.Context
 import tice.exceptions.GMSUnavailableException
 
-fun RegisterViewModel.registerViaPush(userName: String?) {
+fun RegisterViewModel.registerViaPush(userName: String?, context: Context) {
     logger.error("Push registering requested but GMS unavailable.")
     throw GMSUnavailableException
 }
