3b0aa1b9f556513bcdab81966392114cd41f9d62
Fabio Tacke <fabio@tacke.berlin>
2021-11-10 16:44:39 +0100

    Fix failing checks

--- app/src/androidTest/java/tice/ui/activitys/StartSharingTest.kt
+++ app/src/androidTest/java/tice/ui/activitys/StartSharingTest.kt
@@ -28,6 +28,6 @@ class StartSharingTest {
         
         waitForView(allOf(withContentDescription("Google Map"), isDisplayed())).perform(longClick())
         waitForView(allOf(withId(R.id.bottom_sheet), isDisplayed()))
-        waitForView(allOf(withText("Set as Meeting Point"), isDisplayed())).perform(click())
+        waitForView(allOf(withText(R.string.map_createMeetingPoint), isDisplayed())).perform(click())
     }
 }
